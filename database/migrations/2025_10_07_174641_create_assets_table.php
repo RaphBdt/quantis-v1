a8ce09e512c9e9682a640a1573335dffea8b4dbc
Raphael Beaudet <raphaelbeaudet@icloud.com>
2025-10-27 20:04:07 +0100

    Asset model and migration

---
+++ database/migrations/2025_10_07_174641_create_assets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Scenario;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['cash', 'real_estate', 'stocks', 'cryptocurrency', 'other']);
+            $table->decimal('net_worth', 19)->default(0);
+            $table->decimal('yield', 19)->default(0);
+            $table->decimal('monthly_investment', 19)->default(0);
+            $table->decimal('monthly_rent', 19)->default(0);
+            $table->foreignIdFor(Scenario::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assets');
+    }
+};
