823a9eade5a6351a5fe8f6447e80c6a4b17a751d
Raphael Beaudet <raphaelbeaudet@icloud.com>
2025-11-05 20:11:04 +0100

    template layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,10 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title')</title>
+    <title>Quantis | @yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    @yield('content')
+<body class="bg-gray-900">
+    <div class="min-h-full">
+        <div class="py-10">
+            <header>
+                <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <h1 class="text-3xl font-bold tracking-tight text-white">@yield('title')</h1>
+                </div>
+            </header>
+            <main>
+                <div class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+                    @yield('content')
+                </div>
+            </main>
+        </div>
+    </div>
 </body>
 </html>
