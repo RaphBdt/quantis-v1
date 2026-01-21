3a17938239ae85344242bd18a2a66c7fe51b87ab
Raphael Beaudet <raphaelbeaudet@icloud.com>
2026-01-21 19:44:12 +0100

    checkbox component

---
+++ resources/views/shared/checkbox.blade.php
@@ -0,0 +1,21 @@
+@php
+$name ??= '';
+$label ??= ucfirst($name);
+$value ??= false;
+$class ??= null;
+@endphp
+
+<div @class(['flex gap-3', $class])>
+    <div class="flex h-6 shrink-0 items-center">
+        <div class="group grid size-4 grid-cols-1">
+            <input id="{{ $name }}" type="checkbox" name="{{ $name }}" @if($value) checked @endif class="col-start-1 row-start-1 appearance-none rounded-sm border border-white/10 bg-white/5 checked:border-indigo-500 checked:bg-indigo-500 indeterminate:border-indigo-500 indeterminate:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 disabled:border-white/5 disabled:bg-white/10 disabled:checked:bg-white/10 forced-colors:appearance-auto" />
+            <svg viewBox="0 0 14 14" fill="none" class="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-white/25">
+                <path d="M3 8L6 11L11 3.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="opacity-0 group-has-checked:opacity-100" />
+                <path d="M3 7H11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="opacity-0 group-has-indeterminate:opacity-100" />
+            </svg>
+        </div>
+    </div>
+    <div class="text-sm/6">
+        <label for="comments" class="font-medium text-white">{{ $label }}</label>
+    </div>
+</div>
