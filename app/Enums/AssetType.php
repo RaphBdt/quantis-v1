074f3f530f11f8d98dbe21e92e4a47940adf6a88
Raphael Beaudet <56366432+RaphBdt@users.noreply.github.com>
2026-07-24 17:33:46 +0200

    add labels to AssetType Enum

--- app/Enums/AssetType.php
+++ app/Enums/AssetType.php
@@ -9,4 +9,23 @@ enum AssetType: string
     case STOCKS = 'stocks';
     case CRYPTOCURRENCY = 'cryptocurrency';
     case OTHER = 'other';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::CASH => 'Cash',
+            self::REAL_ESTATE => 'Real estate',
+            self::STOCKS => 'Stocks',
+            self::CRYPTOCURRENCY => 'Cryptocurrency',
+            self::OTHER => 'Other',
+        };
+    }
+
+    public static function options(): array
+    {
+        return array_combine(
+            array_column(self::cases(), 'value'),
+            array_map(fn(self $case) => $case->label(), self::cases())
+        );
+    }
 }
